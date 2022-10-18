9d57f78f4315f483ebfd87baa23afb17f01561cc
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-18 14:01:56 -0400

    Alternating the text color and background color based on active tab or not

--- uber-eats-react-native/components/headertabs/HeaderTabs.js
+++ uber-eats-react-native/components/headertabs/HeaderTabs.js
@@ -1,18 +1,22 @@
 import { View, Text, Button, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
 
 const HeaderTabs = () => {
+
+  const [activeTab, setActiveTab] = useState("Delivery")
+
   return (
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-      <HeaderButton title="Delivery" btnColor="black" txtColor="white" />
-      <HeaderButton title="Pickup" btnColor="white" txtColor="black" />
+      <HeaderButton title="Delivery" btnColor="black" txtColor="white" activeTab={activeTab} setActiveTab={setActiveTab} />
+      <HeaderButton title="Pickup" btnColor="white" txtColor="black" activeTab={activeTab} setActiveTab={setActiveTab} />
     </View>
   )
 }
 
-const HeaderButton = ({ title, btnColor, txtColor }) => (
+const HeaderButton = ({ title, btnColor, txtColor, activeTab, setActiveTab }) => (
   <View>
-    <TouchableOpacity style={{ backgroundColor: btnColor, paddingVertical: 6, paddingHorizontal: 16, borderRadius: 30 }}>
-      <Text style={{ color: txtColor, fontSize: 14, fontWeight: '900' }}>{title}</Text>
+    <TouchableOpacity onPress={() => setActiveTab(title)} style={{ backgroundColor: activeTab === title ? 'black' : 'white', paddingVertical: 6, paddingHorizontal: 16, borderRadius: 30 }}>
+      <Text style={{ color: activeTab === title ? 'white' : 'black', fontSize: 14, fontWeight: '900' }}>{title}</Text>
     </TouchableOpacity>
   </View>
 )
