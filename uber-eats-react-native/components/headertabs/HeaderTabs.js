d78555bffa7b5d180d1b7ba6e4da61da2aa5ed2f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-18 13:35:31 -0400

    Changing the background color and text color of the 2 buttons

--- uber-eats-react-native/components/headertabs/HeaderTabs.js
+++ uber-eats-react-native/components/headertabs/HeaderTabs.js
@@ -3,16 +3,16 @@ import { View, Text, Button, TouchableOpacity } from 'react-native'
 const HeaderTabs = () => {
   return (
     <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-      <HeaderButton title="Delivery" />
-      <HeaderButton title="Pickup" />
+      <HeaderButton title="Delivery" btnColor="black" txtColor="white" />
+      <HeaderButton title="Pickup" btnColor="white" txtColor="black" />
     </View>
   )
 }
 
-const HeaderButton = ({ title }) => (
+const HeaderButton = ({ title, btnColor, txtColor }) => (
   <View>
-    <TouchableOpacity style={{ backgroundColor: 'black' }}>
-      <Text style={{ color: 'white' }}>{title}</Text>
+    <TouchableOpacity style={{ backgroundColor: btnColor, paddingVertical: 6, paddingHorizontal: 16, borderRadius: 30 }}>
+      <Text style={{ color: txtColor, fontSize: 14, fontWeight: '900' }}>{title}</Text>
     </TouchableOpacity>
   </View>
 )
