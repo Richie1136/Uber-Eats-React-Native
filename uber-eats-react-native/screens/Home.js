abcbdd91fb57b39b6b3987892dad2a7bef1dadd1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-10-19 13:19:23 -0400

    Created SearchBar component and imported it inside of the Home component

--- uber-eats-react-native/screens/Home.js
+++ uber-eats-react-native/screens/Home.js
@@ -1,11 +1,13 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import HeaderTabs from '../components/headertabs/HeaderTabs'
+import SearchBar from '../components/searchbar/SearchBar'
 
 const Home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
       <View style={{ backgroundColor: 'white', padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   )
